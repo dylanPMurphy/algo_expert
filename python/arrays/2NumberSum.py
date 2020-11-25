58179071bbf37d72974518a5714c729f4058d571
mac <dylanmurphyDBA@gmail.com>
2020-11-24 23:04:33 -0800

    2NumberSum

---
+++ python/arrays/2NumberSum.py
@@ -0,0 +1,8 @@
+def twoNumberSum(array, targetSum):
+    # Write your code here.
+	for i in range(0,len(array)-1):
+		for j in range (i+1, len(array)):
+			if array[i] + array[j] == targetSum:
+				return[array[i], array[j]]
+	
+    return []
