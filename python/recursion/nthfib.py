616d73af5f6de4a178a6ceb0829f01a1716b0ffd
mac <dylanmurphyDBA@gmail.com>
2021-01-11 19:23:02 -0800

    memoized!

--- python/recursion/nthfib.py
+++ python/recursion/nthfib.py
@@ -1,8 +1,17 @@
-def getNthFib(n):
-    if n == 2:
-        return 1
-    elif n == 1: 
-        return 0
+# def getNthFib(n):
+#     if n == 2:
+#         return 1
+#     elif n == 1: 
+#         return 0
+#     else:
+#         return getNthFib(n-1)+getNthFib(n-2)
+
+
+def getNthFib(n, memoize = {1: 0, 2: 1}):
+
+    if n in memoize:
+        return memoize[n]
     else:
-        return getNthFib(n-1)+getNthFib(n-2)
-        
+        memoize[n] = getNthFib(n - 1, memoize) + getNthFib(n-2, memoize)
+        return memoize[n]
+    
