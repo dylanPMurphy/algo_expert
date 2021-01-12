7f7c590301b0757223c0becfc1b4301077f83ad2
mac <dylanmurphyDBA@gmail.com>
2021-01-11 19:12:48 -0800

    Got my mins!

--- python/trees/minHeightBST.py
+++ python/trees/minHeightBST.py
@@ -1,15 +1,17 @@
 def minHeightBst(array):
-    return constructMinHeightBST(array, 0, len(array-1))
+    return constructMinHeightBST(array, 0, len(array)-1)
 
-constructMinHeightBST(array, startIndex, endIndex):
+def constructMinHeightBST(array, startIndex, endIndex):
     if endIndex < startIndex:
         return None
     
-    midIndex = (endIndex+ startIndex)//2
+    midIndex = (endIndex + startIndex)//2
     bst = BST(array[midIndex])
-    bst.left = constructMinHeightBST(array, startIndex, midIndex)
-    bst.right = constructMinHeightBST(array, midIndex, endIndex)
+    bst.left = constructMinHeightBST(array, startIndex, midIndex-1)
+    bst.right = constructMinHeightBST(array, midIndex+1, endIndex)
     return bst
+
+
 
 
 class BST:
