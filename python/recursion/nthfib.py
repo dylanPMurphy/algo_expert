a329c051b809d1495874b0223390176daf127fdd
mac <dylanmurphyDBA@gmail.com>
2021-01-11 19:25:54 -0800

    iteration makes complexity smaller

--- python/recursion/nthfib.py
+++ python/recursion/nthfib.py
@@ -15,3 +15,13 @@ def getNthFib(n, memoize = {1: 0, 2: 1}):
         memoize[n] = getNthFib(n - 1, memoize) + getNthFib(n-2, memoize)
         return memoize[n]
     
+    
+def getNthFib(n):
+    lastTwo = [0,1]
+    counter = 3
+    while counter <= n:
+        nextFib = lastTwo[0]+ lastTwo[1]
+        lastTwo[0]=lastTwo[1]
+        lastTwo[1]=nextFib
+        counter+=1
+    return lastTwo[1] if n > 1 else lastTwo[0]
