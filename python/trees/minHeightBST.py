ae580780ab069c91625e80b992d3d7814cc835c6
mac <dylanmurphyDBA@gmail.com>
2021-01-11 19:04:25 -0800

    starting MinHeightBST

---
+++ python/trees/minHeightBST.py
@@ -0,0 +1,24 @@
+def minHeightBst(array):
+    return constructMinHeightBST(array, 0, len(array-1))
+
+constructMinHeightBST(array, startIndex, endIndex):
+
+
+
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                self.left = BST(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = BST(value)
+            else:
+                self.right.insert(value)
