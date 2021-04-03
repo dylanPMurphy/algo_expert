f36288e0b6351b3253c4e86ab2ab529171e58320
Dylan Murphy <dylanmurphydba@gmail.com>
2021-04-03 16:28:33 -0700

    BranchSums Init

---
+++ python/BranchSums.py
@@ -0,0 +1,13 @@
+# This is the class of the input root. Do not edit it.
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+def branchSums(root):
+    # Write your code here.
+    out = []
+	runner = root
+	
